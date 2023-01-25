a9ab754b181e49a37db0ae8d258a7de76fa631e4
Ben Kyd <benjaminkyd@gmail.com>
2023-01-25 18:41:04 +0000

    tmux

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,11 @@ yay -S ttf-twemoji xotf-nerd-fonts-monacob-mono xidlehook sysstat i3blocks mpris
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts
 curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf 
+cd ~
+
+# Tmux
+yay -S tmux 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Manjaro/i3 audio with Yay
 yay -S pulseaudio-equalizer-ladspa mpris-ctl
