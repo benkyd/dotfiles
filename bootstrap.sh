e8ec26806da60617315c4928ddf8f986b0dfe8e8
Ben Kyd <benjaminkyd@gmail.com>
2023-01-25 18:24:34 +0000

    font

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,9 @@ yay -S neovim-git nvm
 # Manjaro/i3 visuals with Yay
 yay -S ttf-twemoji xotf-nerd-fonts-monacob-mono xidlehook sysstat i3blocks mpris-ctl flameshot perl rofi
 
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
+curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf 
 
 # Manjaro/i3 audio with Yay
 yay -S pulseaudio-equalizer-ladspa mpris-ctl
