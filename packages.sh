1de29ebb46b49f46a3734f3a1200e6d4448c97d0
Ben Kyd <benjaminkyd@gmail.com>
2023-01-25 17:29:18 +0000

    add neovim

--- packages.sh
+++ packages.sh
@@ -3,11 +3,12 @@
 # Install the packages that the dotfiles need to function properly
 
 # Dev utils
-yay -S nvm
+yay -S neovim-git nvm
 
 # Manjaro/i3 visuals with Yay
-yay -S ttf-twemoji xidlehook sysstat i3blocks mpris-ctl flameshot perl rofi
+yay -S ttf-twemoji nerd-fonts-complete-starship xidlehook sysstat i3blocks mpris-ctl flameshot perl rofi
 
 
 # Manjaro/i3 audio with Yay
 yay -S pulseaudio-equalizer-ladspa mpris-ctl
+
