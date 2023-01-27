15de8214c40908113ba8cd6aae0e01912a5ad045
Benjamin Kyd <benjamin.kyd@intel.com>
2023-01-27 17:33:00 +0000

    my own vim

---
+++ home/.config/nvim/lua/post-plugin-basics.lua
@@ -0,0 +1,10 @@
+-- ################# Basic settings dependent on plugins ################ --
+
+-- ================= Visualization ================= --
+
+vim.o.termguicolors = true
+vim.o.background = 'dark'
+-- TODO: Catpuchino
+vim.cmd('colorscheme palenight')
+vim.api.nvim_command('let g:palenight_terminal_italics=1')
+
