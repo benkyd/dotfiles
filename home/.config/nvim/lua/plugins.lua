03df806ee6a883a584b4777a571217b62ee2ffe2
Ben Kyd <benjaminkyd@gmail.com>
2023-01-30 00:06:16 +0000

    lol

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -69,6 +69,7 @@ local packer = require('packer').startup(function(use)
   -- better highlighting
   use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
 
+  use 'kyazdani42/nvim-web-devicons'
   -- use {
   --   'kyazdani42/nvim-tree.lua',
   --   requires = 'kyazdani42/nvim-web-devicons',
--- home/.config/nvim/plugin/packer_compiled.lua
+++ home/.config/nvim/plugin/packer_compiled.lua
@@ -169,6 +169,11 @@ _G.packer_plugins = {
     path = "/home/ben/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
+  ["nvim-web-devicons"] = {
+    loaded = true,
+    path = "/home/ben/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    url = "https://github.com/kyazdani42/nvim-web-devicons"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/home/ben/.local/share/nvim/site/pack/packer/start/packer.nvim",
