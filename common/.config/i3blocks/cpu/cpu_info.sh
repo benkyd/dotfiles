3887a71a2fcb39da7dfc5bd5d151f2a79a7da9dd
Ben Kyd <benjaminkyd@gmail.com>
2023-02-06 23:43:24 +0000

    lol

--- common/.config/i3blocks/cpu/cpu_info.sh
+++ common/.config/i3blocks/cpu/cpu_info.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-TEMP=$(sensors | grep 'Package id 0:\|Tdie' | grep ':[ ]*+[0-9]*.[0-9]*°C' -o | grep '+[0-9]*.[0-9]*°C' -o)
-CPU_USAGE=$(mpstat 1 1 | awk '/Average:/ {printf("%s\n", $(NF-9))}')
-echo "$CPU_USAGE $TEMP" | awk '{ printf(" CPU:%6s% @ %s \n"), $1, $2 }'
+TEMP=$(sensors | grep 'Package id 0:\|Tdie' | grep ':[ ]*+[0-9]*.[0-9]*°C' -o | grep '[0-9]*.[0-9]*°C' -o)
+CPU_USAGE=$(mpstat 1 1 | awk '/Average:/ {printf("%i\n", $(NF-9))}')
+echo "$CPU_USAGE $TEMP" | awk '{ printf(" CPU:%3s% @ %s \n"), $1, $2 }'
