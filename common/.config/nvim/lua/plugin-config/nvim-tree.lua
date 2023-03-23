c1275506ed9e04b406bf1b919115b430f292675b
Benjamin Kyd <benjamin.kyd@intel.com>
2023-03-23 10:34:36 +0000

    tree

--- common/.config/nvim/lua/plugin-config/nvim-tree.lua
+++ common/.config/nvim/lua/plugin-config/nvim-tree.lua
@@ -1,7 +1,19 @@
 
 require("nvim-tree").setup({
+    hijack_unnamed_buffer_when_opening = true,
+    open_on_setup = false,
+    open_on_setup_file = false,
+    update_focused_file = {
+        enable = true,
+    },
+    view = {
+        width = 30,
+    },
     git = {
         ignore = false,
     },
+    filters = {
+        dotfiles = false,
+    }
 })
 
