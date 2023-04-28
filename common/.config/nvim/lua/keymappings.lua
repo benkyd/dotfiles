541502187b6616beb7e8d9b7da40320f7a512eb1
Benjamin Kyd <benjamin.kyd@intel.com>
2023-04-28 15:07:29 +0100

    epic

--- common/.config/nvim/lua/keymappings.lua
+++ common/.config/nvim/lua/keymappings.lua
@@ -1,4 +1,4 @@
--- defaults
+    -- defaults
 local opts = { noremap = true, silent = true }
 
 -- paste in insert mode
@@ -11,14 +11,10 @@ vim.api.nvim_set_keymap('', '<leader>p', '"_dP', opts)
 vim.api.nvim_set_keymap('n', '<C-b>', ":NvimTreeToggle<CR>", { noremap = true })
 
 -- make ctrl-shift arrows line movement
-vim.api.nvim_set_keymap('n', '<C-A-Up>', 'ddkP', opts)
-vim.api.nvim_set_keymap('v', '<C-A-Up>', ':m \'<-2<CR>gv=gv', opts)
-vim.api.nvim_set_keymap('n', '<C-A-Down>', 'ddp', opts)
-vim.api.nvim_set_keymap('v', '<C-A-Down>', ':m \'>+1<CR>gv=gv', opts)
-
--- stay in normal mode after inserting a new line
---vim.api.nvim_set_keymap('', 'o', 'o <Bs><Esc>', opts)
---vim.api.nvim_set_keymap('', 'O', 'O <Bs><Esc>', opts)
+vim.api.nvim_set_keymap('n', '<C-S-Up>', 'ddkP', opts)
+vim.api.nvim_set_keymap('v', '<C-S-Up>', ':m \'<-2<CR>gv=gv', opts)
+vim.api.nvim_set_keymap('n', '<C-S-Down>', 'ddp', opts)
+vim.api.nvim_set_keymap('v', '<C-S-Down>', ':m \'>+1<CR>gv=gv', opts)
 
 -- Mapping U to Redo.
 vim.api.nvim_set_keymap('', 'U', '<C-r>', opts)
@@ -31,8 +27,4 @@ vim.api.nvim_set_keymap('v', '<Tab>', '>>_', opts)
 vim.api.nvim_set_keymap('v', '<S-Tab>', '<<_', opts)
 vim.api.nvim_set_keymap('i', '<Tab>', '\t', opts)
 vim.api.nvim_set_keymap('i', '<S-Tab>', '\b', opts)
-
--- jumping back and forth
-vim.api.nvim_set_keymap('', '<C-K>', '<C-O>', opts)
-vim.api.nvim_set_keymap('', '<C-L>', '<C-I>', opts)
 
