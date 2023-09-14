323a444f7a6fb744a161c616b7223b2bf45f5ef5
Ben Kyd <benjaminkyd@gmail.com>
2023-09-14 19:39:22 +0100

    some more configs

--- common/.config/wezterm/wezterm.lua
+++ common/.config/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ end
 
 
 config.hide_tab_bar_if_only_one_tab = true
-config.font = wezterm.font("DejaVu Sans Mono")
+config.font = wezterm.font("JetBrains Mono")
 config.font_size = 10.0
 config.adjust_window_size_when_changing_font_size = false
 
